eaa8981d3b524800be62c0c0d5bb564ee7ed1644
Tsav Samson <tsavsamson2014@gmail.com>
2024-11-29 12:15:27 +0100

    shipping to production

--- houseprediction/settings.py
+++ houseprediction/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,14 +24,16 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static/'
 ]
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x07sk7l$*=6y7r8xxo7do!y72-oec$13*%vq0fs7%w_+b#^uk='
+# SECRET_KEY = 'x07sk7l$*=6y7r8xxo7do!y72-oec$13*%vq0fs7%w_+b#^uk='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
